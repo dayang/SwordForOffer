98724f0768a82a135ce083355185ed640cd43417
yonghua <1306693844@qq.com>
2016-10-06 01:28:33 +0800

    question 28

--- question28.js
+++ question28.js
@@ -4,3 +4,31 @@
 则打印出由字符a,b,c所能排列出来的所有字符串abc,acb,bac,bca,cab
 和cba.
  */
+
+/*
+将第一个字符分别与其它字符兑换，递归从第二个字符开始的字符串
+递归 + 回溯
+ */
+function printPermutation(s){
+    function permutation(sarray, begin){
+        if(begin == sarray.length)
+            console.log(sarray.join(''));
+        else{
+            var temp, i;
+            for(i = begin; i < sarray.length; i++){
+                temp = sarray[i];
+                sarray[i] = sarray[begin];
+                sarray[begin] = temp;
+
+                permutation(sarray,begin + 1);
+
+                temp = sarray[i];
+                sarray[i] = sarray[begin];
+                sarray[begin] = temp;
+            }
+        }
+    }
+    permutation(s.split(""),0);
+}
+
+printPermutation("abcd");
