2ff5223ff8eae6101006358c4b57679449355943
dayang <1306693844@qq.com>
2016-10-08 16:45:43 +0800

    question 21

--- question21.js
+++ question21.js
@@ -4,3 +4,45 @@
 栈的最小元素的min函数。在该栈中，调用min,push
 及pop的时间复杂度都是O(1)
  */
+function MinStack(){
+    this.stack = [];
+    this.minStack = [];
+    this.len = 0;
+}
+
+MinStack.prototype.push = function(val){
+    this.stack.push(val);
+    if(this.len <= 0 || val < this.minStack[this.len - 1]){
+        this.minStack.push(val);
+    }else{
+        this.minStack.push(this.minStack[this.len - 1]);
+    }
+    this.len++;
+};
+
+MinStack.prototype.pop = function(){
+    if(this.len > 0){
+        this.stack.pop();
+        this.minStack.pop();
+        this.len --;
+    }else{
+        throw new Error("stack is empty");
+    }
+};
+
+MinStack.prototype.min = function(){
+    if(this.len <= 0) {
+        throw new Error("stack is empty");
+    }
+    return this.minStack[this.len - 1];
+};
+
+var minStack = new MinStack();
+minStack.push(12);
+minStack.push(3);
+console.log(minStack.min());
+minStack.push(15);
+console.log(minStack.min());
+minStack.pop();
+minStack.pop();
+console.log(minStack.min());
